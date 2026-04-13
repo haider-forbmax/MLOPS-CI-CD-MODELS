c4b9dfa8777159262a39564be7666b91ae1c160b
Haider <haider.ali@betacodespk.com>
2026-04-13 18:12:34 +0500

    used minikube and kaniko again new run again again again..

--- mlops/APIs/fr-api/api.py
+++ mlops/APIs/fr-api/api.py
@@ -1,4 +1,4 @@
-#ttttttttttttTtttest for the git ops"
+#tttttttttttttTtttest for the git ops"
 from fastapi import FastAPI, HTTPException, status, Header, Depends, Request
 from datetime import datetime
 import time
