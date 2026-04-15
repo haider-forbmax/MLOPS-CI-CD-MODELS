3b3bc1225d1768edc1489e1fd84b2603c31cb971
Haider <haider.ali@betacodespk.com>
2026-04-15 11:05:25 +0500

    testtt

--- mlops/APIs/fr-api/api.py
+++ mlops/APIs/fr-api/api.py
@@ -1,4 +1,4 @@
-#ttttttttttttttttttttttttTtttest for the git ops"
+#test for the git ops"
 from fastapi import FastAPI, HTTPException, status, Header, Depends, Request
 from datetime import datetime
 import time
